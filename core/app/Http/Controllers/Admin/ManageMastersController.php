de70f6a56b2811546d2ebf3936a23075d101e5bd
Dimple <dimple.crayon@gmail.com>
2025-01-22 18:11:34 +0530

    Manage Masters -> Add

--- core/app/Http/Controllers/Admin/ManageMastersController.php
+++ core/app/Http/Controllers/Admin/ManageMastersController.php
@@ -102,16 +102,18 @@ class ManageMastersController extends Controller
         }
         return $masters->searchable(['mastername', 'email'])->orderBy('id', 'desc')->paginate(getPaginate());
     }
-    
+
 
     public function detail($id)
     {
         $master = Master::findOrFail($id);
         $pageTitle = 'Master Detail - ' . $master->mastername;
-
-        $totalDeposit = Deposit::where('master_id', $master->id)->successful()->sum('amount');
-        $totalWithdrawals = Withdrawal::where('master_id', $master->id)->approved()->sum('amount');
-        $totalTransaction = Transaction::where('master_id', $master->id)->count();
+        $totalDeposit = 0;
+        $totalWithdrawals = 0;
+        $totalTransaction = 0;
+        // $totalDeposit = Deposit::where('master_id', $master->id)->successful()->sum('amount');
+        // $totalWithdrawals = Withdrawal::where('master_id', $master->id)->approved()->sum('amount');
+        // $totalTransaction = Transaction::where('master_id', $master->id)->count();
         $countries = json_decode(file_get_contents(resource_path('views/partials/country.json')));
         return view('admin.masters.detail', compact('pageTitle', 'master', 'totalDeposit', 'totalWithdrawals', 'totalTransaction', 'countries'));
     }
