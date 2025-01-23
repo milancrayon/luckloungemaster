cc56eef0cc56f6b48da431ae191f9a8b8452f4e0
Dimple <dimple.crayon@gmail.com>
2025-01-23 12:19:23 +0530

    Create new Login for the master.

---
+++ core/resources/views/master/partials/topnav.blade.php
@@ -0,0 +1,146 @@
+@php
+    $sidenav = json_decode($sidenav);
+
+    $settings = file_get_contents(resource_path('views/master/setting/settings.json'));
+    $settings = json_decode($settings);
+
+    $routesData = [];
+    foreach (\Illuminate\Support\Facades\Route::getRoutes() as $route) {
+        $name = $route->getName();
+        if (strpos($name, 'master') !== false) {
+            $routeData = [
+                $name => url($route->uri()),
+            ];
+
+            $routesData[] = $routeData;
+        }
+    }
+@endphp
+
+<!-- navbar-wrapper start -->
+<nav class="navbar-wrapper bg--dark  d-flex flex-wrap">
+    <div class="navbar__left">
+        <button type="button" class="res-sidebar-open-btn me-3"><i class="las la-bars"></i></button>
+        <form class="navbar-search">
+            <input type="search" name="#0" class="navbar-search-field" id="searchInput" autocomplete="off"
+                   placeholder="@lang('Search here...')">
+            <i class="las la-search"></i>
+            <ul class="search-list"></ul>
+        </form>
+    </div>
+    <div class="navbar__right">
+        <ul class="navbar__action-list"> 
+            <li>
+                <button type="button" class="primary--layer" data-bs-toggle="tooltip" data-bs-placement="bottom" title="@lang('Visit Website')">
+                    <a href="{{ route('home') }}" target="_blank"><i class="las la-globe"></i></a>
+                </button>
+            </li>
+            <li class="dropdown">
+                <button type="button" class="primary--layer notification-bell" data-bs-toggle="dropdown" data-display="static"
+                        aria-haspopup="true" aria-expanded="false">
+                    <span data-bs-toggle="tooltip" data-bs-placement="bottom" title="@lang('Unread Notifications')">
+                        <i class="las la-bell @if ($masterNotificationCount > 0) icon-left-right @endif"></i>
+                    </span>
+                    @if ($masterNotificationCount > 0)
+                        <span class="notification-count">{{ $masterNotificationCount <= 9 ? $masterNotificationCount : '9+' }}</span>
+                    @endif
+                </button>
+                <div class="dropdown-menu dropdown-menu--md p-0 border-0 box--shadow1 dropdown-menu-right">
+                    <div class="dropdown-menu__header">
+                        <span class="caption">@lang('Notification')</span>
+                        @if ($masterNotificationCount > 0)
+                            <p>@lang('You have') {{ $masterNotificationCount }} @lang('unread notification')</p>
+                        @endif
+                    </div>
+                    <div class="dropdown-menu__body @if (blank($masterNotifications)) d-flex justify-content-center align-items-center @endif">
+                        @forelse($masterNotifications as $notification)
+                            <a href="{{ route('master.notification.read', $notification->id) }}"
+                               class="dropdown-menu__item">
+                                <div class="navbar-notifi">
+                                    <div class="navbar-notifi__right">
+                                        <h6 class="notifi__title">{{ __($notification->title) }}</h6>
+                                        <span class="time"><i class="far fa-clock"></i>
+                                            {{ diffForHumans($notification->created_at) }}</span>
+                                    </div>
+                                </div>
+                            </a>
+                        @empty
+                            <div class="empty-notification text-center">
+                                <img src="{{ getImage('assets/images/empty_list.png') }}" alt="empty">
+                                <p class="mt-3">@lang('No unread notification found')</p>
+                            </div>
+                        @endforelse
+                    </div>
+                    <div class="dropdown-menu__footer">
+                        <a href="{{ route('master.notifications') }}"
+                           class="view-all-message">@lang('View all notifications')</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <button type="button" class="primary--layer" data-bs-toggle="tooltip" data-bs-placement="bottom" title="@lang('System Setting')">
+                    <a href="{{ route('master.setting.system') }}"><i class="las la-wrench"></i></a>
+                </button>
+            </li>
+            <li class="dropdown d-flex profile-dropdown">
+                <button type="button" data-bs-toggle="dropdown" data-display="static" aria-haspopup="true"
+                        aria-expanded="false">
+                    <span class="navbar-user">
+                        <span class="navbar-user__thumb"><img src="{{ getImage(getFilePath('masterProfile') . '/' . auth()->guard('master')->user()->image, getFileSize('masterProfile')) }}" alt="image"></span>
+                        <span class="navbar-user__info">
+                            <span class="navbar-user__name">{{ auth()->guard('master')->user()->username }}</span>
+                        </span>
+                        <span class="icon"><i class="las la-chevron-circle-down"></i></span>
+                    </span>
+                </button>
+                <div class="dropdown-menu dropdown-menu--sm p-0 border-0 box--shadow1 dropdown-menu-right">
+                    <a href="{{ route('master.profile') }}"
+                       class="dropdown-menu__item d-flex align-items-center px-3 py-2">
+                        <i class="dropdown-menu__icon las la-user-circle"></i>
+                        <span class="dropdown-menu__caption">@lang('Profile')</span>
+                    </a>
+
+                    <a href="{{ route('master.password') }}"
+                       class="dropdown-menu__item d-flex align-items-center px-3 py-2">
+                        <i class="dropdown-menu__icon las la-key"></i>
+                        <span class="dropdown-menu__caption">@lang('Password')</span>
+                    </a>
+
+                    <a href="{{ route('master.logout') }}" class="dropdown-menu__item d-flex align-items-center px-3 py-2">
+                        <i class="dropdown-menu__icon las la-sign-out-alt"></i>
+                        <span class="dropdown-menu__caption">@lang('Logout')</span>
+                    </a>
+                </div>
+                <button type="button" class="breadcrumb-nav-open ms-2 d-none">
+                    <i class="las la-sliders-h"></i>
+                </button>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!-- navbar-wrapper end -->
+
+@push('script')
+    <script>
+        "use strict";
+        var routes = @json($routesData);
+        var settingsData = Object.assign({}, @json($settings), @json($sidenav));
+
+        $('.navbar__action-list .dropdown-menu').on('click', function(event) {
+            event.stopPropagation();
+        });
+    </script>
+    <script src="{{ asset('assets/master/js/search.js') }}"></script>
+    <script>
+        "use strict";
+
+        function getEmptyMessage() {
+            return `<li class="text-muted">
+                <div class="empty-search text-center">
+                    <img src="{{ getImage('assets/images/empty_list.png') }}" alt="empty">
+                    <p class="text-muted">No search result found</p>
+                </div>
+            </li>`
+        }
+    </script>
+@endpush
