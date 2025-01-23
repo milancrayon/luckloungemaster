4e6115c892944f564e421725c6595af70f6c0e11
Dimple <dimple.crayon@gmail.com>
2025-01-23 18:25:21 +0530

    Master -> Customer Management

--- core/resources/views/admin/masters/detail.blade.php
+++ core/resources/views/admin/masters/detail.blade.php
@@ -119,7 +119,7 @@
                             <div class="form-group">
                                 <label>@lang('Exposure')</label>
                                 <div class="input-group">
-                                    <input type="number" step="any" name="exposure" class="form-control" placeholder="@lang('Please provide positive exposure')" disabled value="0">
+                                    <input type="number" step="any" name="exposure" class="form-control" placeholder="@lang('Please provide positive exposure')" value="0">
                                 </div>
                             </div>
                         </div>
