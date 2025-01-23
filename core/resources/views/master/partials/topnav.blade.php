e235a078252a6d7b2e3e9dfc4d399252931be7b9
Dimple <dimple.crayon@gmail.com>
2025-01-23 12:59:04 +0530

    Create new Login for the master.

--- core/resources/views/master/partials/topnav.blade.php
+++ core/resources/views/master/partials/topnav.blade.php
@@ -1,20 +1,20 @@
 @php
-    $sidenav = json_decode($sidenav);
+$sidenav = json_decode($sidenav);
 
-    $settings = file_get_contents(resource_path('views/admin/setting/settings.json'));
-    $settings = json_decode($settings);
+$settings = file_get_contents(resource_path('views/admin/setting/settings.json'));
+$settings = json_decode($settings);
 
-    $routesData = [];
-    foreach (\Illuminate\Support\Facades\Route::getRoutes() as $route) {
-        $name = $route->getName();
-        if (strpos($name, 'master') !== false) {
-            $routeData = [
-                $name => url($route->uri()),
-            ];
+$routesData = [];
+foreach (\Illuminate\Support\Facades\Route::getRoutes() as $route) {
+$name = $route->getName();
+if (strpos($name, 'master') !== false) {
+$routeData = [
+$name => url($route->uri()),
+];
 
-            $routesData[] = $routeData;
-        }
-    }
+$routesData[] = $routeData;
+}
+}
 @endphp
 
 <!-- navbar-wrapper start -->
@@ -23,26 +23,22 @@
         <button type="button" class="res-sidebar-open-btn me-3"><i class="las la-bars"></i></button>
         <form class="navbar-search">
             <input type="search" name="#0" class="navbar-search-field" id="searchInput" autocomplete="off"
-                   placeholder="@lang('Search here...')">
+                placeholder="@lang('Search here...')">
             <i class="las la-search"></i>
             <ul class="search-list"></ul>
         </form>
     </div>
     <div class="navbar__right">
-        <ul class="navbar__action-list"> 
+        <ul class="navbar__action-list">
             <li>
                 <button type="button" class="primary--layer" data-bs-toggle="tooltip" data-bs-placement="bottom" title="@lang('Visit Website')">
                     <a href="{{ route('home') }}" target="_blank"><i class="las la-globe"></i></a>
                 </button>
             </li>
-            <li>
-                <button type="button" class="primary--layer" data-bs-toggle="tooltip" data-bs-placement="bottom" title="@lang('System Setting')">
-                    <a href="{{ route('master.setting.system') }}"><i class="las la-wrench"></i></a>
-                </button>
-            </li>
+
             <li class="dropdown d-flex profile-dropdown">
                 <button type="button" data-bs-toggle="dropdown" data-display="static" aria-haspopup="true"
-                        aria-expanded="false">
+                    aria-expanded="false">
                     <span class="navbar-user">
                         <span class="navbar-user__thumb"><img src="{{ getImage(getFilePath('masterProfile') . '/' . auth()->guard('master')->user()->image, getFileSize('masterProfile')) }}" alt="image"></span>
                         <span class="navbar-user__info">
@@ -53,13 +49,13 @@
                 </button>
                 <div class="dropdown-menu dropdown-menu--sm p-0 border-0 box--shadow1 dropdown-menu-right">
                     <a href="{{ route('master.profile') }}"
-                       class="dropdown-menu__item d-flex align-items-center px-3 py-2">
+                        class="dropdown-menu__item d-flex align-items-center px-3 py-2">
                         <i class="dropdown-menu__icon las la-user-circle"></i>
                         <span class="dropdown-menu__caption">@lang('Profile')</span>
                     </a>
 
                     <a href="{{ route('master.password') }}"
-                       class="dropdown-menu__item d-flex align-items-center px-3 py-2">
+                        class="dropdown-menu__item d-flex align-items-center px-3 py-2">
                         <i class="dropdown-menu__icon las la-key"></i>
                         <span class="dropdown-menu__caption">@lang('Password')</span>
                     </a>
@@ -79,26 +75,26 @@
 <!-- navbar-wrapper end -->
 
 @push('script')
-    <script>
-        "use strict";
-        var routes = @json($routesData);
-        var settingsData = Object.assign({}, @json($settings), @json($sidenav));
+<script>
+    "use strict";
+    var routes = @json($routesData);
+    var settingsData = Object.assign({}, @json($settings), @json($sidenav));
 
-        $('.navbar__action-list .dropdown-menu').on('click', function(event) {
-            event.stopPropagation();
-        });
-    </script>
-    <script src="{{ asset('assets/master/js/search.js') }}"></script>
-    <script>
-        "use strict";
+    $('.navbar__action-list .dropdown-menu').on('click', function(event) {
+        event.stopPropagation();
+    });
+</script>
+<script src="{{ asset('assets/master/js/search.js') }}"></script>
+<script>
+    "use strict";
 
-        function getEmptyMessage() {
-            return `<li class="text-muted">
+    function getEmptyMessage() {
+        return `<li class="text-muted">
                 <div class="empty-search text-center">
                     <img src="{{ getImage('assets/images/empty_list.png') }}" alt="empty">
                     <p class="text-muted">No search result found</p>
                 </div>
             </li>`
-        }
-    </script>
+    }
+</script>
 @endpush
