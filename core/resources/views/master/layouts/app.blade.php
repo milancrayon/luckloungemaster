cca588e3319b8f00d212405eaa688176f802dff1
Dimple <dimple.crayon@gmail.com>
2025-01-23 13:02:04 +0530

    Create new Login for the master.

--- core/resources/views/master/layouts/app.blade.php
+++ core/resources/views/master/layouts/app.blade.php
@@ -1,24 +1,24 @@
 @extends('master.layouts.master')
 @section('content')
-    @php
-        $sidenav = file_get_contents(resource_path('views/admin/partials/sidenav.json'));
-    @endphp
-    <!-- page-wrapper start -->
-    <div class="page-wrapper default-version">
-        @include('master.partials.sidenav')
-        @include('master.partials.topnav')
+@php
+$sidenav = file_get_contents(resource_path('views/master/partials/sidenav.json'));
+@endphp
+<!-- page-wrapper start -->
+<div class="page-wrapper default-version">
+    @include('master.partials.sidenav')
+    @include('master.partials.topnav')
 
-        <div class="container-fluid px-3 px-sm-0">
-            <div class="body-wrapper">
-                <div class="bodywrapper__inner">
+    <div class="container-fluid px-3 px-sm-0">
+        <div class="body-wrapper">
+            <div class="bodywrapper__inner">
 
-                    @stack('topBar')
-                    @include('master.partials.breadcrumb')
+                @stack('topBar')
+                @include('master.partials.breadcrumb')
 
-                    @yield('panel')
+                @yield('panel')
 
-                </div><!-- bodywrapper__inner end -->
-            </div><!-- body-wrapper end -->
-        </div>
+            </div><!-- bodywrapper__inner end -->
+        </div><!-- body-wrapper end -->
     </div>
+</div>
 @endsection
