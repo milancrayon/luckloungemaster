0ede12bc7475c8ca46e05b78046b9ff6e90c1a59
Dimple <dimple.crayon@gmail.com>
2025-01-23 15:25:37 +0530

    Master -> Customer Management

--- core/app/Providers/AppServiceProvider.php
+++ core/app/Providers/AppServiceProvider.php
@@ -68,6 +68,22 @@ class AppServiceProvider extends ServiceProvider
             ]);
         });
 
+        view()->composer('master.partials.sidenav', function ($view) {
+            $view->with([
+                'bannedMastersCount'           => User::banned()->count(),
+                'emailUnverifiedMastersCount'  => User::emailUnverified()->count(),
+                'mobileUnverifiedMastersCount' => User::mobileUnverified()->count(),
+                'kycUnverifiedMastersCount'    => User::kycUnverified()->count(),
+                'kycPendingMastersCount'       => User::kycPending()->count(),
+                'pendingTicketCount'         => SupportTicket::whereIN('status', [Status::TICKET_OPEN, Status::TICKET_REPLY])->count(),
+                'pendingDepositsCount'       => Deposit::pending()->count(),
+                'pendingWithdrawCount'       => Withdrawal::pending()->count(),
+                'pendingBetCount'            => Bet::pending()->count(),
+                'bannedMastersCount'           => Master::banned()->count(),
+            ]);
+        });
+
+
         view()->composer('admin.partials.topnav', function ($view) {
             $view->with([
                 'adminNotifications'     => AdminNotification::where('is_read', Status::NO)->with('user')->orderBy('id', 'desc')->take(10)->get(),
