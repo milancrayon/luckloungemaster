249fa7af3916e4f3215c1a4f0cf8a8e4786c2451
Dimple <dimple.crayon@gmail.com>
2025-01-23 15:51:20 +0530

    Master -> Customer Management

---
+++ core/resources/views/master/customers/add.blade.php
@@ -0,0 +1,196 @@
+@extends('admin.layouts.app')
+
+@section('panel')
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title mb-0">@lang('Information of')</h5>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('admin.masters.store') }}" method="POST"
+                    enctype="multipart/form-data">
+                    @csrf
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label>@lang('Master Name')</label>
+                                <input class="form-control" type="text" name="mastername" required value="">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label>@lang('First Name')</label>
+                                <input class="form-control" type="text" name="firstname" required value="">
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label class="form-control-label">@lang('Last Name')</label>
+                                <input class="form-control" type="text" name="lastname" required value="">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>@lang('Email') </label>
+                                <input class="form-control" type="email" name="email" value="" required>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>@lang('Mobile Number') </label>
+                                <div class="input-group ">
+                                    <span class="input-group-text mobile-code">+</span>
+                                    <input type="number" name="mobile" value="" id="mobile" class="form-control checkUser" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="form-group ">
+                                <label>@lang('Address')</label>
+                                <input class="form-control" type="text" name="address" value="">
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-md-6">
+                            <div class="form-group">
+                                <label>@lang('City')</label>
+                                <input class="form-control" type="text" name="city" value="">
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-md-6">
+                            <div class="form-group ">
+                                <label>@lang('State')</label>
+                                <input class="form-control" type="text" name="state" value="">
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-md-6">
+                            <div class="form-group ">
+                                <label>@lang('Zip/Postal')</label>
+                                <input class="form-control" type="text" name="zip" value="">
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-md-6">
+                            <div class="form-group ">
+                                <label>@lang('Country')<span class="text--danger">*</span></label>
+                                <select name="country" class="form-control select2">
+                                    @foreach ($countries as $key => $country)
+                                    <option data-mobile_code="{{ $country->dial_code }}" value="{{ $key }}">{{ __($country->country) }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="form-group">
+                                <label>@lang('Amount')</label>
+                                <div class="input-group">
+                                    <input type="number" step="any" name="amount" class="form-control" placeholder="@lang('Please provide positive amount')" required>
+                                    <div class="input-group-text">{{ __(gs('cur_text')) }}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="form-group">
+                                <label>@lang('Exposure')</label>
+                                <div class="input-group">
+                                    <input type="number" step="any" name="exposure" class="form-control" placeholder="@lang('Please provide positive exposure')" disabled value="0">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="form-group">
+                                <label>@lang('Password')</label>
+                                <div class="input-group">
+                                    <input type="password" name="password" class="form-control" placeholder="@lang('New Password')" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="form-group">
+                                <label>@lang('Password Confirm')</label>
+                                <div class="input-group">
+                                    <input type="password" name="password_confirmation" class="form-control" placeholder="@lang('Confirmed Password')" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 col-12">
+                            <div class="form-group">
+                                <label>@lang('Email Verification')</label>
+                                <input type="checkbox" data-width="100%" data-onstyle="-success" data-offstyle="-danger"
+                                    data-bs-toggle="toggle" data-on="@lang('Verified')" data-off="@lang('Unverified')" name="ev">
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-md-6 col-12">
+                            <div class="form-group">
+                                <label>@lang('Mobile Verification')</label>
+                                <input type="checkbox" data-width="100%" data-onstyle="-success" data-offstyle="-danger"
+                                    data-bs-toggle="toggle" data-on="@lang('Verified')" data-off="@lang('Unverified')" name="sv">
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md- col-12">
+                            <div class="form-group">
+                                <label>@lang('2FA Verification') </label>
+                                <input type="checkbox" data-width="100%" data-height="50" data-onstyle="-success" data-offstyle="-danger" data-bs-toggle="toggle" data-on="@lang('Enable')" data-off="@lang('Disable')" name="ts">
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md- col-12">
+                            <div class="form-group">
+                                <label>@lang('KYC') </label>
+                                <input type="checkbox" data-width="100%" data-height="50" data-onstyle="-success" data-offstyle="-danger" data-bs-toggle="toggle" data-on="@lang('Verified')" data-off="@lang('Unverified')" name="kv">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn--primary w-100 h-45">@lang('Submit')
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@push('breadcrumb-plugins')
+
+@endpush
+
+@push('script')
+<script>
+    (function($) {
+        "use strict"
+        $(document).ready(function() {
+            // Trigger the 'change' event on the country select element
+            $('select[name=country]').trigger('change');
+        });
+        $('.bal-btn').on('click', function() {
+
+            $('.balanceAddSub')[0].reset();
+
+            var act = $(this).data('act');
+            $('#addSubModal').find('input[name=act]').val(act);
+            if (act == 'add') {
+                $('.type').text('Add');
+            } else {
+                $('.type').text('Subtract');
+            }
+        });
+
+        let mobileElement = $('.mobile-code');
+        $('select[name=country]').on('change', function() {
+            mobileElement.text(`+${$('select[name=country] :selected').data('mobile_code')}`);
+        });
+
+    })(jQuery);
+</script>
+@endpush
