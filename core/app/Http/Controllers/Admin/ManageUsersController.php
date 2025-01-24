589b646124646ff07cf8b87ea82ecfdff1f4ba60
Dimple <dimple.crayon@gmail.com>
2025-01-24 17:26:33 +0530

    Admin -> User -> Master wiser filter in user

--- core/app/Http/Controllers/Admin/ManageUsersController.php
+++ core/app/Http/Controllers/Admin/ManageUsersController.php
@@ -120,7 +120,7 @@ class ManageUsersController extends Controller
     protected function masterData()
     {
         $masters = Master::query();
-        return $masters->orderBy('id', 'desc')->paginate(getPaginate());
+        return $masters->orderBy('id', 'desc');
     }
 
 
