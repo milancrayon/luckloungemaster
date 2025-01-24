789dad91589d5686643d9dacb4e893dc4f6d57d7
Dimple <dimple.crayon@gmail.com>
2025-01-24 18:19:04 +0530

    Game -> Filter

--- core/app/Http/Controllers/Admin/GameController.php
+++ core/app/Http/Controllers/Admin/GameController.php
@@ -110,26 +110,10 @@ class GameController extends Controller
     {
         $pageTitle = "Game Logs";
 
-        $logs      = GameLog::where('status', Status::ENABLE)->searchable(['user:username,firstname,lastname,email', 'game:name'])->filter(['win_status'])->with('user', 'game')->latest('id')
-            ->paginate(getPaginate());
-
-        $searchTerm = request('search'); // Get search term from the request
-        $logs = GameLog::where('game_logs.status', Status::ENABLE)
-            ->join('users', 'game_logs.user_id', '=', 'users.id')
-            ->join('games', 'game_logs.game_id', '=', 'games.id')
-            ->where(function ($query) use ($searchTerm) {
-                $query->where('users.username', 'like', '%' . $searchTerm . '%')
-                    ->orWhere('users.firstname', 'like', '%' . $searchTerm . '%')
-                    ->orWhere('users.lastname', 'like', '%' . $searchTerm . '%')
-                    ->orWhere('users.email', 'like', '%' . $searchTerm . '%')
-                    ->orWhere('games.name', 'like', '%' . $searchTerm . '%');
-            })
-            ->filter(['win_status']) // Apply win_status filter
-            ->with('user', 'game') // Eager load relationships
-            ->latest('game_logs.id')
-            ->paginate(getPaginate());
-        // $sql = $logs->toSql();
-        // dd($sql);
+        $logs      = GameLog::where('status', Status::ENABLE)->searchable(['user:username', 'user:firstname', 'user:lastname', 'user:email', 'game:email'])->filter(['win_status'])->with('user', 'game')->latest('id');
+        // ->paginate(getPaginate());
+        $sql = $logs->toSql();
+        dd($sql);
         return view('admin.game.log', compact('pageTitle', 'logs'));
     }
 
