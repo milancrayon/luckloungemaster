01b9f76c165768240c960620fc6129c01203c8a0
Dimple <dimple.crayon@gmail.com>
2025-01-24 17:05:36 +0530

    Admin -> User -> Master wiser filter in user

--- core/app/Http/Controllers/Admin/ManageUsersController.php
+++ core/app/Http/Controllers/Admin/ManageUsersController.php
@@ -109,8 +109,8 @@ class ManageUsersController extends Controller
         } else {
             $users = User::query();
         }
-        $inputValue = $scope->input('master_id');
-        print_r($inputValue);
+        $inputValue = request()->input('master_id');
+        print_r($inputValue);  // For debugging purposes, prints the value of 'master_id'
         exit();
         $users->where('created_by', $user->id);
         return $users->searchable(['username', 'email'])->orderBy('id', 'desc')->paginate(getPaginate());
