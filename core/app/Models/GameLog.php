d728c96590fb258325852dfe27790e23d1ebd1ba
Dimple <dimple.crayon@gmail.com>
2025-01-24 17:49:21 +0530

    Game -> Filter

--- core/app/Models/GameLog.php
+++ core/app/Models/GameLog.php
@@ -5,22 +5,40 @@ namespace App\Models;
 use App\Constants\Status;
 use Illuminate\Database\Eloquent\Model;
 
-class GameLog extends Model {
+class GameLog extends Model
+{
+    use Searchable;
+
+    // Define searchable columns (the columns that will be indexed for search)
+    public function toSearchableArray()
+    {
+        return [
+            'username' => $this->user->username, // Assuming a relationship to 'user' model
+            'email' => $this->user->email,
+            'lastname' => $this->user->lastname,
+            'firstname' => $this->user->firstname,
+            'game_name' => $this->game->name, // Assuming a relationship to 'game' model
+        ];
+    }
 
     protected $guarded = ['id'];
 
-    public function game() {
+    public function game()
+    {
         return $this->belongsTo(Game::class);
     }
 
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function scopeWin() {
+    public function scopeWin()
+    {
         return $this->where('win_status', Status::WIN);
     }
-    public function scopeLoss() {
+    public function scopeLoss()
+    {
         return $this->where('win_status', Status::LOSS);
     }
 }
