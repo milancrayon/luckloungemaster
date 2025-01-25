609b390a1a4576585f76feef6212961fd3bf76ca
Dimple <dimple.crayon@gmail.com>
2025-01-25 13:45:57 +0530

    Issue display error message before game start (right now in some game display message after game start)

--- assets/global/js/game/aviator.js
+++ assets/global/js/game/aviator.js
@@ -756,7 +756,7 @@ function gameover(lastint) {
 function gamegenerate() {
     $("#auto_increment_number_div").hide();
     $('.loading-game').addClass('show');
-    hide_loading_game();
+   
     for (let i = 0; i < bet_array.length; i++) {
         bet_array[i].game_id = current_game_data.id;
     }
