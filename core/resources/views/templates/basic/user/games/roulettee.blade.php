d70813f8f8f28c857ff3db5fc9c1de083e24327e
Dimple <dimple.crayon@gmail.com>
2025-01-25 13:17:54 +0530

    Issue display error message before game start (right now in some game display message after game start)

--- core/resources/views/templates/basic/user/games/roulettee.blade.php
+++ core/resources/views/templates/basic/user/games/roulettee.blade.php
@@ -1,191 +1,169 @@
 @extends($activeTemplate . 'layouts.master')
 @section('content')
 @push('style-lib')
-
+    
 @endpush
-@push('script-lib')
-<script src="{{ asset('assets/global/js/game/jquery-3.2.1.min.js') }}"></script>
-<script src="{{ asset('assets/global/js/game/createjs.min.js') }}"></script>
-<script src="{{ asset('assets/global/js/game/howler.min.js') }}"></script>
-<script src="{{ asset('assets/global/js/game/roulettee.js') }}"></script>
+@push('script-lib') 
+    <script src="{{ asset('assets/global/js/game/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('assets/global/js/game/createjs.min.js') }}"></script>
+    <script src="{{ asset('assets/global/js/game/howler.min.js') }}"></script>
+    <script src="{{ asset('assets/global/js/game/roulettee.js') }}"></script>
 @endpush
 
 @push('script')
-<script>
-    $(document).ready(function() {
-        var oMain = new CMain({
-            money: {
-                {
-                    auth() - > user() - > balance
-                }
-            }, //STARING CREDIT FOR THE USER
-            min_bet: {
-                {
-                    $game - > min_limit
-                }
-            }, //MINIMUM BET
-            max_bet: {
-                {
-                    $game - > max_limit
-                }
-            }, //MAXIMUM BET
-            time_bet: 0, //TIME TO WAIT FOR A BET IN MILLISECONDS. SET 0 IF YOU DON'T WANT BET LIMIT
-            time_winner: 3000, //TIME FOR WINNER SHOWING IN MILLISECONDS    
-            win_occurrence: {
-                {
-                    $game - > probable_win
-                }
-            }, //Win occurrence percentage (100 = always win). 
-            //SET THIS VALUE TO -1 IF YOU WANT WIN OCCURRENCE STRICTLY RELATED TO PLAYER BET ( SEE DOCUMENTATION)
-            casino_cash: 4000, //The starting casino cash that is recharged by the money lost by the user
-            fullscreen: false, //SET THIS TO FALSE IF YOU DON'T WANT TO SHOW FULLSCREEN BUTTON
-            check_orientation: false, //SET TO FALSE IF YOU DON'T WANT TO SHOW ORIENTATION ALERT ON MOBILE DEVICES
-            show_credits: false, //SET THIS VALUE TO FALSE IF YOU DON'T TO SHOW CREDITS BUTTON
-            num_hand_before_ads: 10 //NUMBER OF HANDS TO COMPLETE, BEFORE TRIGGERING SAVE_SCORE EVENT. USEFUL FOR INTER-LEVEL AD EVENTUALLY.
-        });
-
-
-        $(oMain).on("recharge", function(evt) {});
-
-        $(oMain).on("start_session", function(evt) {
-            if (getParamValue('ctl-arcade') === "true") {
-                parent.__ctlArcadeStartSession();
-            }
-        });
-
-        $(oMain).on("end_session", function(evt) {
-            if (getParamValue('ctl-arcade') === "true") {
-                parent.__ctlArcadeEndSession();
-            }
-            $.ajaxSetup({
-                headers: {
-                    "X-CSRF-TOKEN": "{{ csrf_token() }}",
-                }
+    <script>
+        $(document).ready(function () {
+            var oMain = new CMain({
+                money: {{auth()->user()->balance}},      //STARING CREDIT FOR THE USER
+                min_bet: {{$game->min_limit}},     //MINIMUM BET
+                max_bet: {{$game->max_limit}},     //MAXIMUM BET
+                time_bet: 0,  //TIME TO WAIT FOR A BET IN MILLISECONDS. SET 0 IF YOU DON'T WANT BET LIMIT
+                time_winner: 3000, //TIME FOR WINNER SHOWING IN MILLISECONDS    
+                win_occurrence: {{$game->probable_win}}, //Win occurrence percentage (100 = always win). 
+                //SET THIS VALUE TO -1 IF YOU WANT WIN OCCURRENCE STRICTLY RELATED TO PLAYER BET ( SEE DOCUMENTATION)
+                casino_cash: 4000,    //The starting casino cash that is recharged by the money lost by the user
+                fullscreen: false,     //SET THIS TO FALSE IF YOU DON'T WANT TO SHOW FULLSCREEN BUTTON
+                check_orientation: false, //SET TO FALSE IF YOU DON'T WANT TO SHOW ORIENTATION ALERT ON MOBILE DEVICES
+                show_credits: false,           //SET THIS VALUE TO FALSE IF YOU DON'T TO SHOW CREDITS BUTTON
+                num_hand_before_ads: 10  //NUMBER OF HANDS TO COMPLETE, BEFORE TRIGGERING SAVE_SCORE EVENT. USEFUL FOR INTER-LEVEL AD EVENTUALLY.
             });
-            var url = '{{ route('
-            user.play.roulettebetupdate ') }}';
-            var data = {
-                win: 0,
-            };
-            $.post(url, data, function(response) {
-                if (response.status) {
 
-                } else {
-                    notify("error", response.message);
-                    oMain.gotoMenu();
-                }
-            });
-        });
 
-        $(oMain).on("before_bet_place", function(evt, iTotBet, w) {
-            w.disableBetFiches(), w.enableSpin(!0)
-            $.ajaxSetup({
-                headers: {
-                    "X-CSRF-TOKEN": "{{ csrf_token() }}",
-                }
+            $(oMain).on("recharge", function (evt) { 
             });
-            var url = '{{ route('
-            user.play.roulettebetvalidation ') }}';
-            var data = {
-                invest: iTotBet,
-            };
-            $.post(url, data, function(response) {
-                $(s_oMain).trigger("bet_validation_response", response);
-                if (response.status) {
-                    w.enableBetFiches(), w.enableSpin(1)
-                } else {
-                    w.enableBetFiches(), w.enableSpin(!0)
-                    notify("error", response.message);
-                    oMain.gotoMenu();
-                    $(oMain).trigger("end_session");
-                    $(oMain).trigger("share_event");
-                }
-            });
-        });
 
-        $(oMain).on("bet_placed", function(evt, iTotBet) {
-            $.ajaxSetup({
-                headers: {
-                    "X-CSRF-TOKEN": "{{ csrf_token() }}",
-                }
+            $(oMain).on("start_session", function (evt) {
+                if (getParamValue('ctl-arcade') === "true") {
+                    parent.__ctlArcadeStartSession();
+                } 
             });
-            var url = '{{ route('
-            user.play.roulettebet ') }}';
-            var data = {
-                invest: iTotBet,
-            };
-            $.post(url, data, function(response) {
-                if (response.status) {
-                    notify("success", "Bet Placed!!");
-                } else {
-                    notify("error", response.message);
-                    oMain.gotoMenu();
-                    $(oMain).trigger("end_session");
-                    $(oMain).trigger("share_event");
-                }
-            });
-        });
 
-        $(oMain).on("save_score", function(evt, iMoney, iWin) {
-            if (getParamValue('ctl-arcade') === "true") {
-                parent.__ctlArcadeSaveScore({
-                    score: iMoney
-                });
-            }
-            $.ajaxSetup({
-                headers: {
-                    "X-CSRF-TOKEN": "{{ csrf_token() }}",
-                }
-            });
-            var url = '{{ route('
-            user.play.roulettebetupdate ') }}';
-            var data = {
-                win: iWin,
-            };
-            $.post(url, data, function(response) {
-                if (response.status) {
-                    if (iWin > 0) {
-                        $(".win-loss-popup").addClass("active");
-                        $(".win-loss-popup__body").find("img").addClass("d-none");
-                        $(".win-loss-popup__body").find(".win").removeClass("d-none");
-                        $(".win-loss-popup__footer").find(".data-result").text("You Win " + iWin);
+            $(oMain).on("end_session", function (evt) {
+                if (getParamValue('ctl-arcade') === "true") {
+                    parent.__ctlArcadeEndSession();
+                } 
+                $.ajaxSetup({
+                    headers: {
+                        "X-CSRF-TOKEN": "{{ csrf_token() }}",
                     }
-                } else {
-                    notify("error", response.message);
-                    oMain.gotoMenu();
-                    $(oMain).trigger("end_session");
-                    $(oMain).trigger("share_event");
-                }
+                });
+                var url = '{{ route('user.play.roulettebetupdate') }}';
+                var data = {
+                    win:0, 
+                };
+                $.post(url, data, function(response) { 
+                    if(response.status){
+
+                    }else{
+                        notify("error", response.message);
+                        oMain.gotoMenu();  
+                    }
+                });
             });
 
-        });
-
-        $(oMain).on("show_interlevel_ad", function(evt) {
-            if (getParamValue('ctl-arcade') === "true") {
-                parent.__ctlArcadeShowInterlevelAD();
-            }
-        });
-
-        $(oMain).on("share_event", function(evt, iMoney) {
-            if (getParamValue('ctl-arcade') === "true") {
-                parent.__ctlArcadeShareEvent({
-                    img: "200x200.jpg",
-                    title: TEXT_CONGRATULATIONS,
-                    msg: TEXT_SHARE_1 + iMoney + TEXT_SHARE_2,
-                    msg_share: TEXT_SHARE_3 + iMoney + TEXT_SHARE_4
+            $(oMain).on("before_bet_place", function (evt, iTotBet, w) {  
+                w.disableBetFiches(), w.enableSpin(!0)
+                $.ajaxSetup({
+                    headers: {
+                        "X-CSRF-TOKEN": "{{ csrf_token() }}",
+                    }
                 });
+                var url = '{{ route('user.play.roulettebetvalidation') }}';
+                var data = {
+                    invest:iTotBet, 
+                };
+                $.post(url, data, function(response) {
+                    $(s_oMain).trigger("bet_validation_response", response); 
+                    if(response.status){ 
+                        w.enableBetFiches(), w.enableSpin(!0)
+                    }else{
+                        w.enableBetFiches(), w.enableSpin(!0)
+                        notify("error", response.message);
+                        oMain.gotoMenu();
+                        $(oMain).trigger("end_session");
+                        $(oMain).trigger("share_event");
+                    }
+                });
+            });
+
+            $(oMain).on("bet_placed", function (evt, iTotBet) {  
+                $.ajaxSetup({
+                    headers: {
+                        "X-CSRF-TOKEN": "{{ csrf_token() }}",
+                    }
+                });
+                var url = '{{ route('user.play.roulettebet') }}';
+                var data = {
+                    invest:iTotBet, 
+                };
+                $.post(url, data, function(response) { 
+                    if(response.status){ 
+                        notify("success", "Bet Placed!!");
+                    }else{
+                        notify("error", response.message);
+                        oMain.gotoMenu();
+                        $(oMain).trigger("end_session");
+                        $(oMain).trigger("share_event");
+                    }
+                });
+            });
+
+            $(oMain).on("save_score", function (evt, iMoney, iWin) {
+                if (getParamValue('ctl-arcade') === "true") {
+                    parent.__ctlArcadeSaveScore({ score: iMoney });
+                }  
+                $.ajaxSetup({
+                    headers: {
+                        "X-CSRF-TOKEN": "{{ csrf_token() }}",
+                    }
+                });
+                var url = '{{ route('user.play.roulettebetupdate') }}';
+                var data = {
+                    win:iWin, 
+                };
+                $.post(url, data, function(response) { 
+                    if(response.status){
+                        if(iWin > 0){
+                            $(".win-loss-popup").addClass("active");
+                            $(".win-loss-popup__body").find("img").addClass("d-none"); 
+                            $(".win-loss-popup__body").find(".win").removeClass("d-none");
+                            $(".win-loss-popup__footer").find(".data-result").text("You Win "+iWin);
+                        }
+                    }else{
+                        notify("error", response.message);
+                        oMain.gotoMenu();
+                        $(oMain).trigger("end_session");
+                        $(oMain).trigger("share_event");
+                    }
+                });
+
+            });
+
+            $(oMain).on("show_interlevel_ad", function (evt) {
+                if (getParamValue('ctl-arcade') === "true") {
+                    parent.__ctlArcadeShowInterlevelAD();
+                } 
+            });
+
+            $(oMain).on("share_event", function (evt, iMoney) {
+                if (getParamValue('ctl-arcade') === "true") {
+                    parent.__ctlArcadeShareEvent({
+                        img: "200x200.jpg",
+                        title: TEXT_CONGRATULATIONS,
+                        msg: TEXT_SHARE_1 + iMoney + TEXT_SHARE_2,
+                        msg_share: TEXT_SHARE_3 + iMoney + TEXT_SHARE_4
+                    });
+                } 
+            });
+
+            if (isIOS()) {
+                setTimeout(function () { sizeHandler(); }, 200);
+            } else {
+                sizeHandler();
             }
         });
 
-        if (isIOS()) {
-            setTimeout(function() {
-                sizeHandler();
-            }, 200);
-        } else {
-            sizeHandler();
-        }
-    });
-</script>
+    </script>
 @endpush
 
 <section class="pt-120 pb-120 roulettegame_wrapper">
@@ -194,7 +172,7 @@
         <div id="block_game"
             style="position: fixed; background-color: transparent; top: 0px; left: 0px; width: 100%; height: 100%; display:none">
         </div>
-        <x-recent-game-log :mylogs=$mylogs :alllogs=$alllogs />
+        <x-recent-game-log :mylogs=$mylogs :alllogs=$alllogs /> 
     </div>
 </section>
 
