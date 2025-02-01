1b20caa7df8b41896bbc31a6d6fbda4551fd2d45
Dimple <dimple.crayon@gmail.com>
2025-02-01 16:32:07 +0530

    Update SEO file for Keywords

--- core/resources/views/partials/seo.blade.php
+++ core/resources/views/partials/seo.blade.php
@@ -12,3 +12,16 @@
 @endphp
 
 <meta name="title" Content="{{ gs()->sitename(__($pageTitle)) }}">
+@if(Request::is('user/play/game/roulettee'))
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
+<meta name="msapplication-tap-highlight" content="no" />
+@else
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@endif
+@if ($seoContents)
+    <meta name="description" content="{{ $seoContents->meta_description ?? $seoContents->description }}">
+    <meta name="keywords" content="{{ $seoContents->keywords  }}">
+    <link rel="shortcut icon" href="{{ siteFavicon() }}" type="image/x-icon">
+
+ 
+@endif
