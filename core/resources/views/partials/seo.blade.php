c8a40571e3a84818f11700b7a423f1d773a717d6
Dimple <dimple.crayon@gmail.com>
2025-02-01 16:31:06 +0530

    Update SEO file for Keywords

--- core/resources/views/partials/seo.blade.php
+++ core/resources/views/partials/seo.blade.php
@@ -11,7 +11,6 @@
     }
 @endphp
 
-<!-- <meta name="title" Content="{{ gs()->sitename(__($pageTitle)) }}"> -->
 @if(Request::is('user/play/game/roulettee'))
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
 <meta name="msapplication-tap-highlight" content="no" />
@@ -19,8 +18,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 @endif
 @if ($seoContents)
-    <!-- <meta name="description" content="{{ $seoContents->meta_description ?? $seoContents->description }}">
-    <meta name="keywords" content="{{ $seoContents->keywords  }}"> -->
+    <meta name="description" content="{{ $seoContents->meta_description ?? $seoContents->description }}">
+    <meta name="keywords" content="{{ $seoContents->keywords  }}">
     <link rel="shortcut icon" href="{{ siteFavicon() }}" type="image/x-icon">
 
     {{--
@@ -28,17 +27,16 @@
     <link rel="apple-touch-icon" href="{{ siteLogo() }}">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <!-- <meta name="apple-mobile-web-app-title" content="{{ gs()->sitename($pageTitle) }}"> -->
+
     {{--
     <!-- Google / Search Engine Tags --> --}}
-    <!-- <meta itemprop="name" content="{{ gs()->sitename($pageTitle) }}"> -->
-    <!-- <meta itemprop="description" content="{{ $seoContents->description }}">
-    <meta itemprop="image" content="{{ $seoContents->image }}"> -->
+    <meta itemprop="description" content="{{ $seoContents->description }}">
+    <meta itemprop="image" content="{{ $seoContents->image }}">
     {{--
     <!-- Facebook Meta Tags --> --}}
     <meta property="og:type" content="website">
-    <!-- <meta property="og:title" content="{{ $seoContents->social_title }}">
-    <meta property="og:description" content="{{ $seoContents->social_description }}"> -->
+    <meta property="og:title" content="{{ $seoContents->social_title }}">
+    <meta property="og:description" content="{{ $seoContents->social_description }}">
     <meta property="og:image" content="/{{ getFilePath('seo') ."/". $seoContents->image }}" />
     @php
         if(isset(pathinfo($seoContents->image)['extension'])){
